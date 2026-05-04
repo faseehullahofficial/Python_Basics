f5f96694b2970d3ab5c0c7749bcb608b70401668
faseehullahofficial <faseehullahofficials@gmail.com>
2026-05-04 15:43:00 +0500

    Python code

---
+++ Basics.py
@@ -0,0 +1,6 @@
+# print('Your Learning Path: \n -Python Basics \n\t -AI \n\t Data Engineering')
+x = 10
+y = 9
+print(x+y)
+x = 2
+print(x+y)
---
+++ DataTypes.py
@@ -0,0 +1,22 @@
+# a = 10  # int
+# b = 2.7  # float
+# c = "Hello"  # st
+# e = "1234"  # st
+# f = True
+# # boolean
+# g = False
+# h = None  # none
+# i = ""  # stblank
+# j = " "  # stempty
+
+text = "hi"
+num = 20
+
+print(type(text))
+
+
+print(len(text))
+
+print(bin(num))
+
+print(num.bit_length())
